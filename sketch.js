184369b7a3ba7a30d40e13cbaf4aef78e5762f09
EpicAaruTHEpro <69062625+EpicAaruTHEpro@users.noreply.github.com>
2020-11-08 20:56:58 -0500

    Created the real car game

--- sketch.js
+++ sketch.js
@@ -9,7 +9,16 @@ var database;
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
+var car1Img, car2Img, car3Img, car4Img, groundImg, trackImg;
 
+function preload() {
+  car1Img = loadImage("images/car1.png");
+  car2Img = loadImage("images/car2.png");
+  car3Img = loadImage("images/car3.png");
+  car4Img = loadImage("images/car4.png");
+  groundImg = loadImage("images/ground.png");
+  trackImg = loadImage("images/track.jpg");
+}
 
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
